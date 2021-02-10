cf8659a4278f7d125d105e23899af8bfbbe7be0d
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2021-02-10 03:21:04 +0100

    (NOT WORKING) First try at saving IP address with electron-json-storage

--- src/electronApp/angularApp/components/connect-form/connect-form.component.ts
+++ src/electronApp/angularApp/components/connect-form/connect-form.component.ts
@@ -1,23 +1,35 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { PrinterService } from '../../services/printerService';
-import { ErrorLogger } from '../../../core/errorLogger';
-import { FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
-import { ErrorStateMatcher} from '@angular/material/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { PrinterService } from "../../services/printerService";
+import { ErrorLogger } from "../../../core/errorLogger";
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from "@angular/forms";
+import { ErrorStateMatcher } from "@angular/material/core";
+import { Router, ActivatedRoute } from "@angular/router";
+const storage = window.require("electron-json-storage");
 
 /**
  * Error matcher for teh printer address.
  */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
-
   /**
    * Checks if a control is in an error state.
    * @param control The control to check.
    * @param form The form the control is in.
    */
-  public isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  public isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
     const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
   }
 }
 
@@ -25,9 +37,9 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
  * The connect form component, shown when not connected to a printer.
  */
 @Component({
-  selector: 'app-connect-form',
-  templateUrl: './connect-form.component.html',
-  styleUrls: ['./connect-form.component.css']
+  selector: "app-connect-form",
+  templateUrl: "./connect-form.component.html",
+  styleUrls: ["./connect-form.component.css"],
 })
 export class ConnectFormComponent implements OnInit {
   /**
@@ -45,12 +57,23 @@ export class ConnectFormComponent implements OnInit {
    */
   public matcher = new MyErrorStateMatcher();
 
+  GetLastIP(): string {
+    storage.get("lastIP", function (error, data) {
+      if (error) {
+        ErrorLogger.NonFatalError(error);
+        return "Error";
+      } else {
+        console.log("GET DATA   " + data.lastIP);
+        return data.lastIP;
+      }
+    });
+    return "Nothing from storage or so";
+  }
+
   /**
    * Gets the printer address form.
    */
-  PrinterAddress = new FormControl('', [
-    Validators.required
-  ]);
+  PrinterAddress = new FormControl("", [Validators.required]);
 
   /**
    * Initializes a new instance of the ConnectFormComponent.
@@ -58,12 +81,15 @@ export class ConnectFormComponent implements OnInit {
    * @param printerService The printer service.
    * @param router The Angular router.
    */
-  constructor(private route: ActivatedRoute, private printerService: PrinterService, private router: Router){
-
-    this.printerService.ConnectionStateChanged.Register(isConnected => {
+  constructor(
+    private route: ActivatedRoute,
+    private printerService: PrinterService,
+    private router: Router
+  ) {
+    this.printerService.ConnectionStateChanged.Register((isConnected) => {
       // If we now how a connection, continue to our original destination
-      if (isConnected){
-        this.router.navigate([this.returnUrl ?? '/']);
+      if (isConnected) {
+        this.router.navigate([this.returnUrl ?? "/"]);
       }
     });
   }
@@ -72,9 +98,24 @@ export class ConnectFormComponent implements OnInit {
    * Invoked when the Angular component is initialized.
    */
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       this.returnUrl = params.returnUrl as string;
     });
+  }
+
+  ngAfterViewInit(): void {
+    let temp = "not from storage";
+    storage.get("lastIP", function (error, data) {
+      if (error) {
+        ErrorLogger.NonFatalError(error);
+        temp = "Error";
+      } else {
+        // console.log("GET DATA   " + data.lastIP);
+        temp = data.lastIP;
+      }
+    });
+    console.log("temp log: " + temp);
+    this.PrinterAddress.setValue(temp);
   }
 
   /**
@@ -85,10 +126,16 @@ export class ConnectFormComponent implements OnInit {
       return;
     }
 
-    try{
+    try {
       await this.printerService.ConnectAsync(this.PrinterAddress.value);
-    }
-    catch (e) {
+      storage.set(
+        "lastIP",
+        { lastIP: this.PrinterAddress.value },
+        function (error) {
+          if (error) throw error;
+        }
+      );
+    } catch (e) {
       this.isError = true;
       ErrorLogger.NonFatalError(e);
     }
